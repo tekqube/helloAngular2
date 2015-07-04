683a33c9f0027d9c5f1b2bba2777494f5c69428a
Tekqube <tekqube@gmail.com>
2015-07-04 11:02:51 -0700

    Adding User Form to get information + multiple views in angularJS 2.0

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 var HelloAngular = function() {
-    
+    this.message = "Hello Angular2 (ES5)";
+    this.msgs = ["hi", "ola", "hello", "amigos"];
 };
 
 HelloAngular.annotations = [
     new angular.ComponentAnnotation({
-        selector: 'hello'    
+        selector: 'hello'
     }),
     new angular.ViewAnnotation({
-        template: 'Hello Angular2 (ES5)'    
+        template:
+        '<p>{{message}}</p>' +
+        '<p> Messages : </p>' +
+        '<ul>' +
+        '<li *ng-for="#msg of msgs">'+
+        '{{msg}}'+
+        '</li>'+
+        '</ul>',
+        directives: [angular.NgFor]
     })
+];
+
+var UserForm = function() {
+    this.user = {};
+    this.user.first_name = "";
+    this.user.last_name = "";
+    this.user.phone = "";
+    this.user.email_address = "";
+}
+
+UserForm.prototype.saveUserInfo = function() {
+    console.log('User Info:'+this.user);
+};
+
+UserForm.annotations = [
+  new angular.Component({
+    selector: 'userForm'
+  }),
+  new angular.View({
+    templateUrl : 'userForm.html',
+      directives: [angular.formDirectives]
+  })
 ];
 
 document.addEventListener("DOMContentLoaded", function() {
     angular.bootstrap(HelloAngular);
+    angular.bootstrap(UserForm);
 });
